6a77f37603761d5e8ea4aa85f1dbb7c8b167052d
TERAfenine <138286124+TERAfenine@users.noreply.github.com>
2026-06-12 19:50:43 +0900

    Added Player animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,17 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed = 0;
     [SerializeField] private float jumpForce = 0;
     [SerializeField] Rigidbody2D rigid;
+    [SerializeField] private Animator animator;
 
     private bool isGrounded = false;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
     }
 
     void Start()
@@ -22,7 +27,21 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         float HAxis = Input.GetAxisRaw("Horizontal");
-        rigid.linearVelocity = new Vector2(speed * Input.GetAxisRaw("Horizontal"), rigid.linearVelocity.y);
+        rigid.linearVelocity = new Vector2(speed * HAxis, rigid.linearVelocity.y);
+
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(speed * HAxis));
+        }
+
+        if (HAxis < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (HAxis > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
 
         if (isGrounded) 
         {
@@ -43,6 +62,14 @@ public class Player : MonoBehaviour
             float length = 0.55f;
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, length, layerMask);
             isGrounded = hit;
+
+            if (isGrounded)
+            {
+                if (animator != null)
+                {
+                    animator.SetTrigger("Jump");
+                }
+            }
         }
     }
 }
